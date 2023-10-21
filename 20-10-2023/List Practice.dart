d60c931f486b19cfab06211c83a21fc7dc806926
muzzammalrazzaq <muzzammalrazzaqofficial@gmail.com>
2023-10-21 10:44:05 +0500

    20-10-2023

--- 20-10-2023/List Practice.dart
+++ 20-10-2023/List Practice.dart
@@ -1,4 +1,5 @@
 void main(){
+
   //list with all kind of element
   List Student = [5074 , "Musharaf Ali" , 5075 , "Muzzamal Razzaq" , 5076 , "Ahmad Murad"];
   print(Student);
@@ -43,8 +44,35 @@ void main(){
   t7.sort();
   print(t7);
 
-  //list sort , int
+  //add value in list
+  List team7 = ["shayan", "mudasir"];
+  print(team7);
+  team7.add("nasir");
+  print(team7);
+  team7.addAll(["usama" , "hamid" , "muzzamal"]);
+  print(team7);
+
+  //insert value in list on specific index
+  team7.insert(team7.length-1, "musharaf");
+  print(team7);
+
+  //reversed list element
+  var noinlist;
+  var inlist;
+  noinlist = team7.reversed; // check output bracket , mean it is not a list now
+  inlist = List.of(team7.reversed); // check output bracket , mean it is a list now
+  print(noinlist);
+  print(inlist);
+  
+  //sort list in ascending order , int
   var t7_RN = [5075 , 5074 , 5044 , 5060 , 5051 ,5050 , 5045];
   t7_RN.sort();
   print(t7_RN);
+
+  //sort list in descending order , int
+  //first sort list in ascending order hen apply below method
+  var t7_descending = List.of(t7_RN.reversed);
+  print(t7_descending);
+
+  //remove element from list
 }
