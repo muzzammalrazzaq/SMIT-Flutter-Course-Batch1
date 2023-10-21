85677fbe013b99eb4622366a92b66b60aed81e4a
muzzammalrazzaq <muzzammalrazzaqofficial@gmail.com>
2023-10-21 16:36:27 +0500

    21-10-2023

--- 21-10-2023/Map Practice.dart
+++ 21-10-2023/Map Practice.dart
@@ -1,3 +1,29 @@
 void main(){
-  
+  Map<int , String> studentRecord = {
+    5044 : "shayan",
+    5045 : "mudasir",
+    5050 : "nasir",
+    5051 : "usama",
+    5060 : "hamid",
+    5074 : "musharaf",
+    5075 : "muzzamal",
+  };
+  print(studentRecord);
+  print(studentRecord.keys);
+  print(studentRecord.values);
+
+  //length Method in Map
+  print(studentRecord.length);
+
+  //add or update element in Map
+  studentRecord[5076] = "ahmad";
+  print(studentRecord);
+  studentRecord[5075] = "muzzamal razzaq";
+  print(studentRecord);
+
+  //putIfAbsent method in Map
+  studentRecord.putIfAbsent(5077, () => "bakar");
+  print(studentRecord);
+  studentRecord.putIfAbsent(5075, () => "muzzamal");
+  print(studentRecord);
 }
