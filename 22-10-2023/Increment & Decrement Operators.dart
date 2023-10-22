ba86261a3d684378765250da4478147576a02ff8
muzzammalrazzaq <muzzammalrazzaqofficial@gmail.com>
2023-10-22 10:30:12 +0500

    22-10-2023

---
+++ 22-10-2023/Increment & Decrement Operators.dart
@@ -0,0 +1,23 @@
+void main(){
+  //Pre-Increment Operator
+  var preIncrement = 0;
+  var preIncrement1 = ++preIncrement;
+  print(preIncrement1);
+
+  //Post-Increment Operator
+  var postIncrement = 0;
+  var postIncrement1 = postIncrement++;
+  print(postIncrement);
+  print(postIncrement1);
+
+  //Pre-Decrement Operator
+  var preDecrement = 0;
+  var preDecrement1 = --preDecrement;
+  print(preDecrement1);
+
+  //Post-Decrement Operator
+  var postDecrement = 0;
+  var postDecrement1 = postDecrement--;
+  print(postDecrement);
+  print(postDecrement1);
+}
