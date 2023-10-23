a441abc734d0df72210592bf449feca561ff2b55
muzzammalrazzaq <muzzammalrazzaqofficial@gmail.com>
2023-10-23 08:05:42 +0500

    23-10-2023

--- 23-10-2023/While Loop.dart
+++ 23-10-2023/While Loop.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 void main(){
-  stdout.write("Enter You Email: ");
-  String email = stdin.readLineSync()!;
-  stdout.write("Enter You PassWord: ");
-  String password = stdin.readLineSync()!;
   bool isLogin = false;
 
   while(isLogin==false){
+    stdout.write("Enter You Email: ");
+    String email = stdin.readLineSync()!;
+    stdout.write("Enter You PassWord: ");
+    String password = stdin.readLineSync()!;
     if(email=="admin@gmail.com" && password=="12345"){
       print("Login SuccessFul");
       isLogin = true;
