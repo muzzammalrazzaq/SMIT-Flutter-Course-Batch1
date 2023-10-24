573292bb0d1d469c70c47bbe7ec0ca19dabae3d2
muzzammalrazzaq <muzzammalrazzaqofficial@gmail.com>
2023-10-24 12:19:15 +0500

    24-10-2023

--- 24-10-2023/Functions.dart
+++ 24-10-2023/Functions.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-//Call studentMarksheet1 Function From studentRecord.dart File
+//Calling Functions From studentRecord.dart File
 import 'studentRecord.dart';
 void main(){
   List<int> numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
@@ -13,10 +13,10 @@ void main(){
   var num2 = int.parse(stdin.readLineSync()!);
   print(sumTwoNumber(num1, num2));
 
-//Call studentMarksheet1 Function From studentRecord.dart File
+//Calling studentMarksheet(1 & 2) Function From studentRecord.dart File
   print(studentMarkSheet1("muzzamal", 70, 80, 81));
   print(studentMarkSheet2(name: "muzzamal razzaq", subject1: 70, subject2: 80, subject3: 81));
-
+//Calling studentData Function From studentRecord.dart File
   print(studentData(name: "shayan", rollNO: "5044"));
   print(studentData(name: "sajjad", rollNO: "5035", section: "A"));
   print(studentData(name: "mudasir", rollNO: "5045"));
