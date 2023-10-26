24a63c55ec100c23afbe19573d2c8ad791e09164
muzzammalrazzaq <muzzammalrazzaqofficial@gmail.com>
2023-10-26 09:04:30 +0500

    26-10-2023

--- 26-10-2023/Enum.dart
+++ 26-10-2023/Enum.dart
@@ -1,1 +1,14 @@
-void main(){}
+void main(){
+  var isLogin = info.loginWithEmailAndPassword;
+
+  switch (isLogin) {
+    case info.loginWithEmailAndPassword:
+      print("loginWithEmailAndPassWord");
+      break;
+    default:
+      print("try again");
+  }
+}
+
+//Create Enum
+enum info {loginWithEmailAndPassword , loginWithGoogle , loginWithFaceBook}
